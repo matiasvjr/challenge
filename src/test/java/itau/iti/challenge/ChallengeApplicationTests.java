846dd6fac39b3dcef2da21df75a32317490f7a21
Valdir Matias <matias.valdir@gmail.com>
2020-05-16 01:30:35 -0300

    Initial commit.

---
+++ src/test/java/itau/iti/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package itau.iti.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
